52ac327abba048a94ecb71088891d7fa2855a73e
Antti Haavikko <antti.haavikko@gmail.com>
2024-10-05 11:00:40 +0300

    Show fly route line

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -15,10 +15,12 @@ public class World : MonoBehaviour
     [SerializeField] private GameObject zoomCam;
     [SerializeField] private Menu menu;
     [SerializeField] private GameObject huntCam;
+    [SerializeField] private LineRenderer route;
 
     private Country current;
     private bool flying;
     private bool isMenuFlipped;
+    private bool inCapital;
 
     private void Start()
     {
@@ -27,12 +29,14 @@ public class World : MonoBehaviour
 
     private void Update()
     {
+        UpdateRoute();
         if (Input.GetMouseButtonDown(0)) SetTarget();
         if (DevKey.Down(KeyCode.F)) FlyMode();
     }
 
     public void FlyMode()
     {
+        route.gameObject.SetActive(true);
         menu.Hide();
         zoomCam.SetActive(false);
         flying = true;
@@ -44,9 +48,17 @@ public class World : MonoBehaviour
         menu.Hide();
         this.StartCoroutine(() =>
         {
-            menu.Show(isMenuFlipped);
+            menu.Show(current, inCapital, isMenuFlipped);
             huntCam.SetActive(false);
         }, 2f);
+    }
+
+    private void UpdateRoute()
+    {
+        if (!flying) return;
+        var mp = cam.ScreenToWorldPoint(Input.mousePosition).WhereZ(0);
+        route.SetPosition(0, hunter.transform.position);
+        route.SetPosition(1, mp);
     }
     
     private void SetTarget()
@@ -69,21 +81,25 @@ public class World : MonoBehaviour
             current.Hide();
         }
 
-        var close = Vector3.Distance(mp, closest.CapitalPosition) < 0.5f;
-        var flipped = close && mp.x - closest.CapitalPosition.x > 0;
-        this.StartCoroutine(() => ShowMenu(close ? closest.CapitalPosition : hunter.transform.position, flipped), delay);
+        inCapital = Vector3.Distance(mp, closest.CapitalPosition) < 0.5f;
+        var flipped = inCapital && mp.x - closest.CapitalPosition.x > 0;
+        this.StartCoroutine(() =>
+        {
+            ShowMenu(inCapital ? closest.CapitalPosition : hunter.transform.position, closest, flipped);
+            route.gameObject.SetActive(false);
+        }, delay);
         
-        if (close)
+        if (inCapital)
         {
             this.StartCoroutine(() => closest.Show(), delay);
             current = closest;
         }
     }
 
-    private void ShowMenu(Vector3 pos, bool flip = false)
+    private void ShowMenu(Vector3 pos, Country country = null, bool flip = false)
     {
         isMenuFlipped = flip;
         menu.transform.position = pos;
-        menu.Show(flip);
+        menu.Show(country, inCapital, flip);
     }
 }
